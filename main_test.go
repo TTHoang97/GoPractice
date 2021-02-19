b67ff07197036822577998c117e0e00828af14fd
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2021-02-19 07:12:00 -0500

    Added all tests and basic functionality TODO:Comments.

--- main_test.go
+++ main_test.go
@@ -3,15 +3,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/joho/godotenv"
 )
 
 var a App
-
 
 func TestMain(m *testing.M) {
 
@@ -21,26 +25,190 @@ func TestMain(m *testing.M) {
 		log.Fatal("Error loading .env file")
 	}
 
-    a.Initialize(
-        os.Getenv("APP_DB_USERNAME"),
-        os.Getenv("APP_DB_PASSWORD"),
-        os.Getenv("APP_DB_NAME"))
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
-    ensureTableExists()
-    code := m.Run()
-    clearTable()
-    os.Exit(code)
+	ensureTableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
 }
 
-func ensureTableExists() {
-    if _, err := a.DB.Exec(tableCreationQuery); err != nil {
-        log.Fatal(err)
+//TestEmptyTable - Deletes all records from products tables ands sends GET request to /products
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+/*
+TestGetNonExistentProduct - Tries to access a non-existent product at an endpoint and tests if the response
+is a 404 and and if the response message is "Product not found"
+*/
+func TestGetNonExistentProduct(t *testing.T) {
+    clearTable()
+
+    req, _ := http.NewRequest("GET", "/product/11", nil)
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusNotFound, response.Code)
+
+    var m map[string]string
+    json.Unmarshal(response.Body.Bytes(), &m)
+    if m["error"] != "Product not found" {
+        t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got '%s'", m["error"])
     }
 }
 
+/*
+TestCreateProduct - Adds a product and then fetches it testing if the response code is a 201 and that the 
+response was a JSON object with a correct "Product"
+*/
+func TestCreateProduct(t *testing.T) {
+
+    clearTable()
+
+    var jsonStr = []byte(`{"name":"test product", "price": 11.22}`)
+    req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusCreated, response.Code)
+
+    var m map[string]interface{}
+    json.Unmarshal(response.Body.Bytes(), &m)
+
+    if m["name"] != "test product" {
+        t.Errorf("Expected product name to be 'test product'. Got '%v'", m["name"])
+    }
+
+    if m["price"] != 11.22 {
+        t.Errorf("Expected product price to be '11.22'. Got '%v'", m["price"])
+    }
+
+    // the id is compared to 1.0 because JSON unmarshaling converts numbers to
+    // floats, when the target is a map[string]interface{}
+    if m["id"] != 1.0 {
+        t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+    }
+}
+
+//TestGetProduct - Adds a product to the table and tests that accessing it is successful with response code 200
+func TestGetProduct(t *testing.T) {
+    clearTable()
+    addProducts(1)
+
+    req, _ := http.NewRequest("GET", "/product/1", nil)
+    response := executeRequest(req)
+
+    checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+//addProducts - Creates a new product if the table is empty
+func addProducts(count int) {
+    if count < 1 {
+        count = 1
+    }
+
+    for i := 0; i < count; i++ {
+        a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
+    }
+}
+
+
+/*
+TestUpdateProduct - Updates a created record with new details, should return code 200 and a JSON response
+of the updated product
+*/
+func TestUpdateProduct(t *testing.T) {
+
+    clearTable()
+    addProducts(1)
+
+    req, _ := http.NewRequest("GET", "/product/1", nil)
+    response := executeRequest(req)
+    var originalProduct map[string]interface{}
+    json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+    var jsonStr = []byte(`{"name":"test product - updated name", "price": 11.22}`)
+    req, _ = http.NewRequest("PUT", "/product/1", bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    response = executeRequest(req)
+
+    checkResponseCode(t, http.StatusOK, response.Code)
+
+    var m map[string]interface{}
+    json.Unmarshal(response.Body.Bytes(), &m)
+
+    if m["id"] != originalProduct["id"] {
+        t.Errorf("Expected the id to remain the same (%v). Got %v", originalProduct["id"], m["id"])
+    }
+
+    if m["name"] == originalProduct["name"] {
+        t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalProduct["name"], m["name"], m["name"])
+    }
+
+    if m["price"] == originalProduct["price"] {
+        t.Errorf("Expected the price to change from '%v' to '%v'. Got '%v'", originalProduct["price"], m["price"], m["price"])
+    }
+}
+
+/*
+TestDeleteProduct - Adds a product and deletes it should return status code 200 and checks if that product
+no longer exists
+*/
+func TestDeleteProduct(t *testing.T) {
+    clearTable()
+    addProducts(1)
+
+    req, _ := http.NewRequest("GET", "/product/1", nil)
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusOK, response.Code)
+
+    req, _ = http.NewRequest("DELETE", "/product/1", nil)
+    response = executeRequest(req)
+
+    checkResponseCode(t, http.StatusOK, response.Code)
+
+    req, _ = http.NewRequest("GET", "/product/1", nil)
+    response = executeRequest(req)
+    checkResponseCode(t, http.StatusNotFound, response.Code)
+}
+
+//executeRequest - function to execute to http request
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+//checkResponseCode - checks if the received response code is the expected response code
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func clearTable() {
-    a.DB.Exec("DELETE FROM products")
-    a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
 
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
